f509f0afb295a79603546d3ad24486f421736988
liu-zhih <liuzhih@oregonstate.edu>
2021-10-27 01:37:59 -0700

    fixed type problem

--- src/components/healthBar.js
+++ src/components/healthBar.js
@@ -20,12 +20,8 @@ function HealthBar(props){
         setHealth(health => Math.max(health - damage, 0));
     }
 
-
     return (
         <div className="healthBarContainer">
-            
-            
-
             {/* <p className="healthBar">{health} / {props.health}</p>
             <p className="healthBarB">1</p> */}
             <progress id="health" value={health} max={maxHealth}>{health} / {props.health}</progress>
