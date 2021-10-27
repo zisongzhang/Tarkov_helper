566557d84e28c536016c4117bf6725686556d0a7
liu-zhih <liuzhih@oregonstate.edu>
2021-10-26 19:40:47 -0700

    added button

---
+++ src/components/healthBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HealthBar = () => {
+    return (
+        <div className="healthBar">
+            <button>35/35</button>
+        </div>
+    )
+}
+
+export default HealthBar;
--- src/tarkovhelper.js
+++ src/tarkovhelper.js
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from 'react';
 import {useHistory} from 'react-router-dom';
 import fetch from 'isomorphic-unfetch';
 import {css} from '@emotion/react';
+import HealthBar from './components/healthBar';
 
 function Tarkovhelper(query){
     return(
     <div>
         <h1>Hello world</h1>
         <img src="pic3.png"/>
+        <HealthBar />
     </div>
     );
 }
