98180d788c286063f6f6519486480a1eb251d031
liu-zhih <liuzhih@oregonstate.edu>
2021-11-17 23:09:52 -0800

    fixed health bar problems

--- src/components/HealthBar.js
+++ src/components/HealthBar.js
@@ -4,6 +4,8 @@ import {css} from '@emotion/react';
 import './HealthBar.css';
 
 
+
+
 function HealthBar(props){
 
     const healthBarContainer = css`
@@ -14,7 +16,7 @@ function HealthBar(props){
         left: ${props.left}rem;
     `;
     const [health, setHealth] = useState(props.health);  // set init health
-    const [maxHealth, setMaxHealth] = useState(props.MaxHealth);   // set max health
+    const [maxHealth, setMaxHealth] = useState(props.maxHealth);   // set max health
     const [damage, setDamage] = useState(props.damage);     // damage 
     const [name, setName] = useState(props.name);      // part name
 
@@ -28,11 +30,42 @@ function HealthBar(props){
 
     return (
         <div css = {healthBarContainer}>
-            <progress id="health" value={health} max={maxHealth}>{health} / {props.health}</progress>
-            <p className="healthBar">{health} / {props.health}</p>
+            <progress id="health" value={health} max={maxHealth}>{health} / {maxHealth}</progress>
+            <p className="healthBar">{health} / {maxHealth}</p>
             <button className="healthBarBtn" onClick={()=>doDamage(name)}>{name}</button>
         </div>
     )
 }
+
+// function HealthBar(props){
+
+//     const healthBarContainer = css`
+//         width: 7rem;
+//         margin: 0;
+//         position: absolute;
+//         top: ${props.top}rem;
+//         left: ${props.left}rem;
+//     `;
+//     const [health, setHealth] = useState(props.health);  // set init health
+//     const [maxHealth, setMaxHealth] = useState(props.MaxHealth);   // set max health
+//     const [damage, setDamage] = useState(props.damage);     // damage 
+//     const [name, setName] = useState(props.name);      // part name
+
+//     const doDamage = (part) => {
+//         setHealth(health => Math.max(health - damage, 0));
+        
+//         const tempListRepo = [...props.ListRepo];      // store to local(temp array)
+//         tempListRepo[0][part] = health;     // copy current health to local(temp array)
+//         props.setListRepo(tempListRepo);    //  copy local(temp array) to parent array(ListRepo)
+//     }
+
+//     return (
+//         <div css = {healthBarContainer}>
+//             <progress id="health" value={health} max={maxHealth}>{health} / {maxHealth}</progress>
+//             <p className="healthBar">{health} / {props.health}</p>
+//             <button className="healthBarBtn" onClick={()=>doDamage(name)}>{name}</button>
+//         </div>
+//     )
+// }
 
 export default HealthBar;
